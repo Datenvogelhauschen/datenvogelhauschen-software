6754ac15b9949a4c687fb175121de26e533127ac
jbohnst <92647481+jbohnst@users.noreply.github.com>
2022-01-27 16:04:48 +0100

    FIXED Software crash if online service not reachable

--- src/communication/OnlineService.ts
+++ src/communication/OnlineService.ts
@@ -22,26 +22,31 @@ export class OnlineService {
    * @param tvoc TVOC
    */
   sendSensorData = async (temperature: number, humidity: number, pressure: number, co2: number, tvoc: number) => {
-    const token = OnlineServiceTokenHelper.getCurrentToken();
+    try {
+      const token = OnlineServiceTokenHelper.getCurrentToken();
 
-    const resp = await axios.post(this.apiBaseUrl + "v1/deviceGateway/sensors", {
-      temperature: temperature,
-      humidity: humidity,
-      pressure: pressure,
-      co2: co2,
-      tvoc: tvoc
-    }, {
-      headers: {
-        "Authentication": "Bearer hardwareToken=" + token.hash + "|" + token.timestamp
+      const resp = await axios.post(this.apiBaseUrl + "v1/deviceGateway/sensors", {
+        temperature: temperature,
+        humidity: humidity,
+        pressure: pressure,
+        co2: co2,
+        tvoc: tvoc
+      }, {
+        headers: {
+          "Authentication": "Bearer hardwareToken=" + token.hash + "|" + token.timestamp
+        }
+      });
+
+      if(resp.data.posted) {
+        console.log("Posted current sensor data to online service!");
+        OnlineService.ANY_ERRORS = false;
+      } else {
+        console.log("Error while posting current sensor data to online service!");
+        OnlineService.ANY_ERRORS = true;
       }
-    });
-
-    if(resp.data.posted) {
-      console.log("Posted current sensor data to online service!");
-      OnlineService.ANY_ERRORS = false;
-    } else {
-      console.log("Error while posting current sensor data to online service!");
-      OnlineService.ANY_ERRORS = true;
+    } catch (e) {
+        console.log("Error while posting current sensor data to online service!");
+        OnlineService.ANY_ERRORS = true;
     }
   }
 
@@ -50,23 +55,28 @@ export class OnlineService {
    * @param localPath Local camera image path
    */
   postCurrentCameraImage = async (localPath: string) => {
-    const token = OnlineServiceTokenHelper.getCurrentToken();
+    try {  
+      const token = OnlineServiceTokenHelper.getCurrentToken();
 
-    const form = new FormData();
-    form.append("file", fs.createReadStream(localPath));
+      const form = new FormData();
+      form.append("file", fs.createReadStream(localPath));
 
-    const resp = await axios.post("v1/deviceGateway/camera", form, {
-      headers: {
-        "Authentication": "Bearer hardwareToken=" + token.hash + "|" + token.timestamp
+      const resp = await axios.post("v1/deviceGateway/camera", form, {
+        headers: {
+          "Authentication": "Bearer hardwareToken=" + token.hash + "|" + token.timestamp
+        }
+      });
+
+      if(resp.data.posted) {
+        console.log("Posted current camera data to online service!");
+        OnlineService.ANY_ERRORS = false;
+      } else {
+        console.log("Error while posting current camera data to online service!");
+        OnlineService.ANY_ERRORS = true;
       }
-    });
-
-    if(resp.data.posted) {
-      console.log("Posted current camera data to online service!");
-      OnlineService.ANY_ERRORS = false;
-    } else {
-      console.log("Error while posting current camera data to online service!");
-      OnlineService.ANY_ERRORS = true;
+    } catch (e) {
+        console.log("Error while posting current camera data to online service!");
+        OnlineService.ANY_ERRORS = true;
     }
   }
 }
