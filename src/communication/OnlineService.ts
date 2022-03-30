6e03108946647e1f9105062a60fc96a35130de18
jbohnst <92647481+jbohnst@users.noreply.github.com>
2022-03-30 21:27:06 +0200

    ADDED Error messages no shown when request to online service fails.

--- src/communication/OnlineService.ts
+++ src/communication/OnlineService.ts
@@ -45,11 +45,11 @@ export class OnlineService {
         console.log("Posted current sensor data to online service!");
         OnlineService.ANY_ERRORS = false;
       } else {
-        console.log("Error while posting current sensor data to online service!");
+        console.log("Error while posting current sensor data to online service! Response: " + JSON.stringify(resp));
         OnlineService.ANY_ERRORS = true;
       }
     } catch (e) {
-        console.log("Error while posting current sensor data to online service!");
+        console.log("Error while posting current sensor data to online service: " + e);
         OnlineService.ANY_ERRORS = true;
     }
   }
@@ -83,7 +83,7 @@ export class OnlineService {
         OnlineService.ANY_ERRORS = true;
       }
     } catch (e) {
-        console.log("Error while posting current camera data to online service!");
+        console.log("Error while posting current camera data to online service: " + e);
         OnlineService.ANY_ERRORS = true;
     }
   }
