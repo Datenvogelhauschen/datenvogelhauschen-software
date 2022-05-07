715c5861ae5b55bb506ff404464a136c7c0fce1a
jbohnst <92647481+jbohnst@users.noreply.github.com>
2022-05-07 16:30:51 +0200

    FIXED Device crashes due to reading from all sensors at once
    The Pi seems to be unable keep up with the high energy consumption and crashes

--- src/sensors/Sensor.ts
+++ src/sensors/Sensor.ts
@@ -5,6 +5,7 @@ import {ESensorType} from "./ESensorType";
 
 export class Sensor {
   static READ_INTERVAL: number = 15000;
+  static LAST_SENSOR_DELAY: number = 0;
 
   identifier: string = "unidentified_sensor";
   readableName: string = "Unidentified Sensor";
@@ -22,7 +23,11 @@ export class Sensor {
    * This will execute this.read every _ ms.
    */
   startReadCycle = () => {
-    setInterval(this.read, Sensor.READ_INTERVAL);
+    setTimeout(() => {
+      setInterval(this.read, Sensor.READ_INTERVAL);
+    }, Sensor.LAST_SENSOR_DELAY);
+    
+    Sensor.LAST_SENSOR_DELAY += 2000;
   }
 
   /**
